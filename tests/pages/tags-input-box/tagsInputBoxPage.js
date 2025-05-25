069909ff111f3e22e3b24a3d6eb6090a4c63e909
hernan <hernan97carp@hotmail.com>
2025-05-25 16:25:24 -0300

    refactor: implement POM in tagInputBox tests

--- tests/pages/tags-input-box/tagsInputBoxPage.js
+++ tests/pages/tags-input-box/tagsInputBoxPage.js
@@ -1,7 +1,40 @@
-class TagsInputBoxPage{
-constructor(driver){
-    this.driver = driver;
+exports.TagsInputBoxPage = class TagsInputBoxPage {
+  /**
+   * @param {import('@playwright/test').Page} page
+   */
+  constructor(page) {
+    this.page = page;
+    this.title = page.locator('text="Tags"');
+    this.tags = page.locator('.content >> li');
+    this.input = page.locator('input[type="text"]');
+  }
 
-}
+  async goto() {
+    await this.page.goto('https://qaplayground.dev/apps/tags-input-box/');
+  }
 
-}
+  async tagExists(tagName) {
+    await this.page.locator(`text=${tagName}`)
+  }
+
+  async addTag(tagName) {
+    await this.input.fill(tagName);
+    await this.input.press('Enter');
+  }
+
+  async deleteTagByIndex(index) {
+    await this.tags.nth(index).locator('i').click();
+  }
+
+  async deleteLastTag() {
+    await this.tags.last().locator('i').click();
+  }
+
+  async deleteFirstTag() {
+    await this.tags.first().locator('i').click();
+  }
+
+  async tagCount() {
+    return await this.tags.count();
+  }
+};
--- tests/ui/tags-input-box/tags_inpus_box.test.spec.js
+++ tests/ui/tags-input-box/tags_inpus_box.test.spec.js
@@ -1,76 +1,40 @@
 import { test, expect } from '@playwright/test';
+import { TagsInputBoxPage } from '../../pages/tags-input-box/tagsInputBoxPage.js';
 
 test.describe('Test tags-inputs-box', () => {
+  /** @type {TagsInputBoxPage} */
+  let tagsPage;
+
   test.beforeEach(async ({ page }) => {
-    await page.goto('https://qaplayground.dev/apps/tags-input-box/');
+    tagsPage = new TagsInputBoxPage(page);
+    await tagsPage.goto();
   });
 
-  test('TC1: Verify the existence of the tag called node and javascript.', async ({
-    page,
-  }) => {
-    await expect(page.locator('text="Tags"')).toBeVisible();
-    await expect(page.locator('text=node')).toBeVisible();
-    await expect(page.locator('text=javascript')).toBeVisible();
+  test('TC1: Verify the existence of the tag called node and javascript.', async () => {
+    await expect(tagsPage.title).toBeVisible();
+    await tagsPage.tagExists('node');
+    await tagsPage.tagExists('javascript');
   });
 
-  test('TC2: Remove the tags called node and javascript.', async ({
-    page,
-  }) => {
-    const tags = page.locator('.content >> li');
-    // let countTags = await tags.count();
-    await tags.last().locator('i').click();
-    await tags.first().locator('i').click();
-    await expect(tags).toHaveCount(0);
+  test('TC2: Remove the tags called node and javascript.', async () => {
+    await tagsPage.deleteLastTag();
+    await tagsPage.deleteFirstTag();
+    await expect(tagsPage.tags).toHaveCount(0);
   });
 
-  test('TC3: Add a new tag and delete it afterward', async ({ page }) => {
-    const input = page.locator('input[type="text"]');
-    await input.fill('new tag');
-    await input.press('Enter');
-    await expect(page.locator('text=new tag')).toBeVisible();
-    const lastTag = page.locator('.content >> li').last();
-    await lastTag.locator('i').click();
-    await expect(page.locator('text=new tag')).not.toBeVisible();
+  test('TC3: Add a new tag and delete it afterward', async () => {
+    await tagsPage.addTag('new tag');
+    await tagsPage.tagExists('new tag');
+    await tagsPage.deleteLastTag();
+    await expect(tagsPage.page.locator('text=new tag')).not.toBeVisible();
   });
-  test('TC4: Adds maximum tags (10) with node and javascript pre-existing', async ({
-    page,
-  }) => {
-    const input = page.locator('input[type="text"]');
-    let newTags = [
-      'playwright',
-      'cypress',
-      'python',
-      'selenium',
-      'git',
-      'sql',
-      'postman',
-      'java',
-    ];
-    for (let i = 0; i <= newTags.length - 1; i++) {
-      await input.fill(newTags[i]);
-      await input.press('Enter');
+
+  test('TC4: Adds maximum tags (10) with node and javascript pre-existing', async () => {
+    const extraTags = ['playwright', 'cypress', 'python', 'selenium', 'git', 'sql', 'postman', 'java'];
+    for (const tag of extraTags) {
+      await tagsPage.addTag(tag);
     }
-    const tags = page.locator('.content >> li');
-    expect(page.locator('text="java"')).toBeVisible();
-    expect(await tags.count()).toBe(10);
-  });
-  test.skip('TC5: Adds maximum tags (10) with node and javascript, then deletes all.', async ({
-    page,
-  }) => {
-    let newTags = [
-      'playwright',
-      'cypress',
-      'python',
-      'selenium',
-      'git',
-      'sql',
-      'postman',
-      'java',
-    ];
-    for (let i = 0; i <= newTags.length - 1; i++) {
-      await input.fill(newTags[i]);
-      await input.press('Enter');
-    }
-    expect(await tags.count()).toBe(10);
+    await tagsPage.tagExists('java');
+    expect(await tagsPage.tagCount()).toBe(10);
   });
 });
